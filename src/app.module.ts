8f49d03d45ce62be489f8dd57149f8efa4633df2
NeonChrome93 <daarksky1919@gmail.com>
2023-12-02 19:17:28 +0300

    added local database

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import {ConfigModule} from '@nestjs/config';
 @Module({
     imports: [
         ConfigModule.forRoot(),
-        MongooseModule.forRoot(process.env.MONGO_URL!),
+        MongooseModule.forRoot(process.env.LOCAL_DB ==='true' ?  process.env.LOCAL_MONGO_URL! : process.env.MONGO_URL! ),
         MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}])],
     controllers: [AppController, BlogController,],
     providers: [AppService, BlogService, BlogQueryRepository, BlogRepository],
