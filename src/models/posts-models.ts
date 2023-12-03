4da1dc9582df04887a4a73c452e6700febeed7b1
NeonChrome93 <daarksky1919@gmail.com>
2023-12-03 23:48:55 +0300

    added post-models

---
+++ src/models/posts-models.ts
@@ -0,0 +1,55 @@
+import {WithId} from "mongodb";
+import {REACTIONS_ENUM} from "./comments-models";
+
+
+
+export type PostType = {
+
+    "title": string,
+    "shortDescription": string,
+    "content": string,
+    "blogId": string,
+    "blogName": string,
+    "createdAt": string
+}
+
+export type NewestLikeType = {
+    "addedAt": string,
+    "userId": string,
+    "login": string
+}
+
+export type PostViewType = {
+    "id": string,
+    "title": string,
+    "shortDescription": string,
+    "content": string,
+    "blogId": string,
+    "blogName": string,
+    "createdAt": string,
+    "extendedLikesInfo": {
+        likesCount: number,
+        dislikesCount: number,
+        myStatus: REACTIONS_ENUM,
+        newestLikes:  NewestLikeType[]
+    }
+}
+
+export type mongoTypePost = WithId<PostType>
+
+export type createPostType = {
+    "title": string,
+    "shortDescription": string,
+    "content": string,
+    "blogId": string
+}
+
+
+
+export type PostsQueryType = {
+    pageNumber: string,
+    pageSize: string,
+    sortBy: string,
+    sortDirection: string,
+    searchNameTerm: string | null
+}
