1c11f9fb4848a10e3dc19a4b297872d45edba78c
NeonChrome93 <daarksky1919@gmail.com>
2023-12-25 20:06:32 +0300

    added registration-email-resending.decorator

---
+++ src/common/decorators/registration-email-resending.decorator.ts
@@ -0,0 +1,50 @@
+import {
+    registerDecorator,
+    ValidationOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+    ValidationArguments,
+} from 'class-validator';
+import {UsersRepository} from "../../users/user.repository";
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+@ValidatorConstraint({async: true})
+export class RegistrationEmailResending implements ValidatorConstraintInterface {
+    constructor(private readonly userRepository: UsersRepository) {
+    }
+
+    async validate(email: string, args: ValidationArguments) {
+        const user = await this.userRepository.readUserByEmail(email);
+        if (!user) {
+            return false
+        }
+        if (user.isConfirmed) {
+            return false
+        }
+        return true;
+    }
+
+}
+
+export function IfUserExistOrConfirmed(validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            name: 'IfUserExistOrConfirmed',
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: RegistrationEmailResending,
+        });
+    };
+}
+
+
+// body('email').custom(async (email: string)=> {
+//     const user = await usersRepository.readUserByEmail(email)
+//     if(!user) throw new Error("user not exist")
+//     if(user.isConfirmed) throw new Error("user already confirmed")
+//     return true
+//
+// }),
