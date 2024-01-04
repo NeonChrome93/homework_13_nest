4974e327729146b07dc651b068ebf4b2dbad8c26
NeonChrome93 <daarksky1919@gmail.com>
2024-01-04 23:48:40 +0300

    added comment-owner guard

---
+++ src/guards/comment-owner.guard.ts
@@ -0,0 +1,54 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+    Injectable,
+    NotFoundException,
+    UnauthorizedException
+} from "@nestjs/common";
+import {UserService} from "../users/user.service";
+import {JwtAdapter} from "../common/adapters/jwt.adapter";
+import {CommentsQueryRepository} from "../comments/comment.query.repository";
+import {Request } from 'express'
+
+@Injectable()
+export class CommentOwnerGuard implements CanActivate {
+    constructor(private readonly userService: UserService,
+                private readonly jwtService: JwtAdapter,
+                private readonly commentsQueryRepository: CommentsQueryRepository) {
+    }
+
+    async canActivate(
+        context: ExecutionContext,
+    ): Promise<boolean> {
+        const request  = context.switchToHttp().getRequest<Request>();
+        if (!request.headers.authorization) {
+            throw new UnauthorizedException()
+            return true
+        }
+
+
+
+        const token = request.headers.authorization.split(' ')[1];
+        const userId = await this.jwtService.getUserIdByToken(token)
+        console.log(request.params, 'param')
+        const commentBeforeDelete = await this.commentsQueryRepository.readCommentId(request.params.commentId)
+        console.log(commentBeforeDelete, 'commentBeforeDelete')
+        if (!commentBeforeDelete) {
+            throw new NotFoundException()
+        }
+
+        if (!userId) {
+            throw new NotFoundException()
+        }
+
+        const commentarorId = commentBeforeDelete.commentatorInfo.userId
+        if (commentarorId !== userId.toString()) {
+            throw new ForbiddenException()
+
+        } else {
+            return true;
+        }
+
+    }
+}
