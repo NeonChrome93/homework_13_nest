9668d540567f13a82f8250955046c2a8971df246
NeonChrome93 <daarksky1919@gmail.com>
2024-01-14 23:58:19 +0300

    added tests for device

---
+++ test/security-devices.e2e-spec.ts
@@ -0,0 +1,69 @@
+import {INestApplication} from "@nestjs/common";
+import {Test, TestingModule} from "@nestjs/testing";
+import {AppModule} from "../src/app.module";
+import request from "supertest";
+import {createNewUserModel, createUser, wait} from "./utils";
+import {exec} from "child_process";
+import {appSettings} from "../src/common/config/app.settings";
+
+
+describe('Users API', () => {
+    let app: INestApplication;
+
+
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        appSettings(app)
+        await app.init();
+    });
+
+    it('check user refreshToken to be invalid if it was refreshed before', async () => {
+
+
+        const model = createNewUserModel();
+        const res = await createUser(app, model);
+
+        expect(res).toEqual({
+
+            id: expect.any(String),
+            login: model.login,
+            email: model.email,
+            createdAt: expect.any(String)
+
+        })
+        process.env.REFRESH_TIME = '2s'
+        const res2 = await request(app.getHttpServer()).post('/auth/login').set(
+            'Content-Type', 'application/json'
+        ).send({
+            loginOrEmail: model.login,
+            password: model.password
+        }).expect(200)
+        //
+         await wait(3)
+
+        const res3 = await request(app.getHttpServer()).post('/auth/login').set(
+            'Content-Type', 'application/json'
+        ).send({
+            loginOrEmail: model.login,
+            password: model.password
+        }).expect(200)
+
+
+
+        const refreshToken = res2.headers['set-cookie']
+
+
+
+            const res4 = await request(app.getHttpServer()).post('/auth/refresh-token').set("Cookie", refreshToken).expect(401)
+
+
+
+
+    })
+
+})
